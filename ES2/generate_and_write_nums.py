dc54a48142cb9b80b145cfcd0ecc35e03e23f5bf
Maicon Renildo <63758491+MaiconRenildo@users.noreply.github.com>
2023-04-15 18:02:21 -0300

    Update generate_and_write_nums.py

--- ES2/generate_and_write_nums.py
+++ ES2/generate_and_write_nums.py
@@ -1,5 +1,9 @@
 import random
-nums = random.sample(range(1000000),k=1000000)
-with open('ummilhao.txt','w') as f:
+
+def generate_nums():
+    return random.sample(range(1000000),k=1000000)
+
+def write_nums(nums, filename):
+    with open(filename+'.txt','w') as f:
     for num in nums:  
         f.write('{},'.format(num))
